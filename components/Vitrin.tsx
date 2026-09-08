b9c95b76c1fd42494960e25601a8597269f5a9cc
Ak Emlak <akemlakgayrimenkul15@gmail.com>
2026-09-08 11:25:37 +0300

    bize ulasin gorselini saga yasla

--- components/Vitrin.tsx
+++ components/Vitrin.tsx
@@ -743,7 +743,7 @@ export default function Vitrin() {
             <img
               src="/bize-ulasin.jpg"
               alt="Bize Ulaşın"
-              className="w-full h-full object-contain block rounded-lg"
+              className="w-full h-full object-contain object-right block rounded-lg"
             />
           </div>
         </div>
